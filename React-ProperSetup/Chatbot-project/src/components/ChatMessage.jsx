9bf1f82c0aba1956eb6b417608f5fbd5e28fc74d
Sandeep-Kumar-046 <avnsandeep07@gmail.com>
2025-11-14 19:02:36 +0530

    Realestic UserProfile Image Added to Chat messages

--- React-ProperSetup/Chatbot-project/src/components/ChatMessage.jsx
+++ React-ProperSetup/Chatbot-project/src/components/ChatMessage.jsx
@@ -1,5 +1,5 @@
 import RobotProfileImage from '../assets/robot.png'
-import UserProfileImage from '../assets/user.png'
+import UserProfileImage from '../assets/profile-1.jpg'
 import './ChatMessage.css';
 function ChatMessage(props) // This is an simple example 'Component with props'. Props Make the Component More reuseable.  
 {
