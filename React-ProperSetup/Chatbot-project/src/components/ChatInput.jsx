e30d4bf81c30f43c64c63866dece4bc1c08c7a5c
Sandeep-Kumar-046 <avnsandeep07@gmail.com>
2025-11-14 17:19:28 +0530

    Components are organized

---
+++ React-ProperSetup/Chatbot-project/src/components/ChatInput.jsx
@@ -0,0 +1,75 @@
+import { useState} from "react";
+import { Chatbot } from "supersimpledev";
+import LoadingImage from '../assets/loading-spinner.gif'
+
+
+function ChatInput({chatMessages,setChatMessages}) // This is an component example in React without props
+{
+    const [inputText,setInputText]= useState('');
+    const [isLoading, setIsLoading] = useState(false);
+    function saveInputText(event)
+    {
+        setInputText(event.target.value);
+    }
+    async function sendMessage()
+    {
+        if (isLoading || inputText === '') {
+            return;
+        }
+
+        // Set isLoading to true at the start, and set it to
+        // false after everything is done.
+        setIsLoading(true);
+        const newChatMessages=[
+            ...chatMessages,
+            {
+                message:inputText,
+                sender:"user",
+                id:crypto.randomUUID()
+            },
+            {
+                message: <img src={LoadingImage} className="loading-image"/>,
+                sender: 'robot',
+                id: crypto.randomUUID()
+            }
+        ];
+        setChatMessages(newChatMessages);
+        setInputText('');
+        const response= await Chatbot.getResponseAsync(inputText);
+        setChatMessages([
+            ...newChatMessages.slice(0, newChatMessages.length - 1),
+            {
+                message:response,
+                sender:'robot',
+                id:crypto.randomUUID()
+            },
+        ]);
+        setInputText('');
+        setIsLoading(false);
+    }
+    function handleKeyDown(event)
+    {
+        if(event.key==='Enter')
+        {
+            sendMessage();
+        }else if(event.key==="Escape")
+        {
+            setInputText('');
+        }
+    }
+    return (
+        <div className="chat-input-container">
+            <input placeholder="Send a message to Chatbot" size="30"
+            onChange={saveInputText}
+            onKeyDown={handleKeyDown}
+            value={inputText}
+            className="chat-input"
+            ></input>
+            <button 
+                onClick={sendMessage}
+                className="send-button">Send</button>    
+        </div>
+    );
+}
+
+export default ChatInput
---
+++ React-ProperSetup/Chatbot-project/src/components/ChatMessage.jsx
@@ -0,0 +1,24 @@
+import RobotProfileImage from '../assets/robot.png'
+import UserProfileImage from '../assets/user.png'
+function ChatMessage(props) // This is an simple example 'Component with props'. Props Make the Component More reuseable.  
+{
+    const {message}=props; // same as 'cosnt message = props.message'
+    const {sender}=props;  // This Feature is called Destructuring
+    if (!message) return (null);
+    return( // This return Statement is an short-cut for above if-else statement 
+        <div
+            className={
+                sender==='user' ? "chat-message-user" : "chat-message-robot"
+            }
+        > {/* In JSX We can't able put directly if else statement. Instead of that we can use && operator.*/}
+            {
+                sender === 'robot' && <img src={RobotProfileImage} className="chat-message-profile"/>}
+            <div className="chat-message-text">
+                {message}     
+            </div>
+            {sender === 'user' && <img src={UserProfileImage} className="chat-message-profile"/>}    
+        </div>
+    );
+}
+
+export default ChatMessage
