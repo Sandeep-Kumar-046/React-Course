0684100793dac71e873167a2a75553e36a8bf50e
Sandeep-Kumar-046 <avnsandeep07@gmail.com>
2025-11-18 13:53:28 +0530

    Get cart from the backent and updated the quantity

--- React-ProperSetup/E-Commerce-project/src/pages/HomePage.jsx
+++ React-ProperSetup/E-Commerce-project/src/pages/HomePage.jsx
@@ -5,18 +5,25 @@ import '../components/Header';
 import { Header } from '../components/Header';
 export function HomePage() {
     const [products,setProducts]=useState([]); 
+    const [cart,setCart]=useState([]);
     useEffect(()=>{
         axios.get('http://localhost:3000/api/products')
         .then((response)=>{
           setProducts(response.data);
         })
     },[]);
+    useEffect(()=>{
+        axios.get('http://localhost:3000/api/cart-items')
+            .then((response)=>{
+                setCart(response.data);
+            })
+    });
     
     return (
         <>
             <title>Ecommerce Project</title>
             <link rel="icon" href="home-favicon.png" />
-            <Header />
+            <Header cart={cart} />
             <div className="home-page">
                 <div className="products-grid">
                     {
