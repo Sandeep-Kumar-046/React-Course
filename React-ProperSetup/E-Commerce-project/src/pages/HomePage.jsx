87cde5c88284952d838aa1eb117f9aa5cf608d88
Sandeep-Kumar-046 <avnsandeep07@gmail.com>
2025-11-18 13:35:13 +0530

    Use data from the backend to generate the HTML

--- React-ProperSetup/E-Commerce-project/src/pages/HomePage.jsx
+++ React-ProperSetup/E-Commerce-project/src/pages/HomePage.jsx
@@ -1,13 +1,17 @@
 import axios from 'axios';
+import { useEffect,useState } from 'react';
 import './HomePage.css';
 import '../components/Header';
 import { Header } from '../components/Header';
-import { products } from '../../Starting-Code/data/products';
 export function HomePage() {
-    axios.get('http://localhost:3000/api/products')
+    const [products,setProducts]=useState([]); 
+    useEffect(()=>{
+        axios.get('http://localhost:3000/api/products')
         .then((response)=>{
-           console.log(response.data);
+          setProducts(response.data);
         })
+    },[]);
+    
     return (
         <>
             <title>Ecommerce Project</title>
