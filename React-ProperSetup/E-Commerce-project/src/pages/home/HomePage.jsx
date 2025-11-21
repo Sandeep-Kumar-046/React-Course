27e9f1d9158436e605ed2dd8174bae64fc08a050
Sandeep-Kumar-046 <avnsandeep07@gmail.com>
2025-11-21 17:00:36 +0530

    Search option enabled in the Home Page

--- React-ProperSetup/E-Commerce-project/src/pages/home/HomePage.jsx
+++ React-ProperSetup/E-Commerce-project/src/pages/home/HomePage.jsx
@@ -1,17 +1,21 @@
 import axios from 'axios';
 import { useEffect,useState } from 'react';
+import { useSearchParams } from 'react-router';
 import './HomePage.css';
 import { ProductGrid } from './ProductsGrid';
 import { Header } from '../../components/Header';
 export function HomePage({cart, loadCart}) {
+    const [searchParams] = useSearchParams();
+    const search = searchParams.get('search');
     const [products,setProducts]=useState([]); 
     useEffect(()=>{
         const getHomeData=async ()=>{
-            const response=await axios.get('/api/products')
+            const urlPath = search ? `/api/products?search=${search}` : '/api/products';
+            const response = await axios.get(urlPath);
             setProducts(response.data);
         }
         getHomeData();
-    },[]);
+    },[search]);
     return (
         <>
             <title>Ecommerce Project</title>
